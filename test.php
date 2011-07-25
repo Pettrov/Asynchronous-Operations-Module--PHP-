a9b843058d6ca614d1f32d160893ffb4752b1fef
Geno Roupsky <geno@roupsky.name>
2011-07-25 18:37:47 +0300

    created test suite with all tests

--- test.php
+++ test.php
@@ -1,29 +1,15 @@
 <?php
 
 require 'lib/async.php';
+require 'tests/AsyncManagerTest.php';
 
-class AsyncTest extends PHPUnit_Framework_TestCase {
-
-  private $async;
-  private $storage;
-
-  protected function setUp() {
-    // create a mock storage to simplify testing
-    $this->storage = $this->getMock('Storage', array('add', 'get', 'all', 'pop', 'status', 'result'));
-    $this->assertNotNull($this->storage);
-    $this->async = new AsyncManager($this->storage);
-  }  
-  
-
-  // we test with simple values for job and id, because we want to validate the core framework works
-  public function testAddJob() {
-    $this->storage
-      ->expects($this->once())
-      ->method('add')
-      ->with($this->identicalTo("job"))
-      ->will($this->returnValue(1));
-    
-    $this->assertEquals(1, $this->async->add_job("job"));
-  }
-
+class AllTests {
+    public static function suite() {
+        $suite = new PHPUnit_Framework_TestSuite('All tests');
+      
+        $suite->addTestSuite('AsyncManagerTest');
+        
+        return $suite;
+    }
 }
+
---
+++ tests/AsyncManagerTest.php
@@ -0,0 +1,27 @@
+<?php
+
+class AsyncManagerTest extends PHPUnit_Framework_TestCase {
+
+  private $async;
+  private $storage;
+
+  protected function setUp() {
+    // create a mock storage to simplify testing
+    $this->storage = $this->getMock('Storage', array('add', 'get', 'all', 'pop', 'status', 'result'));
+    $this->assertNotNull($this->storage);
+    $this->async = new AsyncManager($this->storage);
+  }  
+  
+
+  // we test with simple values for job and id, because we want to validate the core framework works
+  public function testAddJob() {
+    $this->storage
+      ->expects($this->once())
+      ->method('add')
+      ->with($this->identicalTo("job"))
+      ->will($this->returnValue(1));
+    
+    $this->assertEquals(1, $this->async->add_job("job"));
+  }
+
+}
